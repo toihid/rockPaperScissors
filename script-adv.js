c78e4131e517fd808ceda90ed564c431e135e958
toihid <it.toihid@gmail.com>
2024-10-13 22:31:17 +0200

    Creating an algorithm for playing like rock, paper scissors
    We have to define a synchronize array of selection
    For example array = ["paper", "rock", "scissors"]
    
    Creating a function that compares two values based on specific rules:
    If the first value is the first item in the array, and the second value is the last item, the second value wins.
    If the first value is the last item in the array, and the second value is the first item, the first value wins.
    Otherwise, the value with the smaller index in the array wins.

---
+++ script-adv.js
@@ -0,0 +1,65 @@
+// tracking the score
+let humanScore = 0;
+let computerScore = 0;
+const CHOICES = ["paper", "rock", "scissors"];
+
+// Function to randomly select the computers choice (rock, paper or Scissors)
+const getComputerChoice = () => {
+  // generate a random index between 0 and 2 to select a choice
+  const randomNumber = Math.floor(Math.random() * 3);
+  console.log(CHOICES[randomNumber]);
+  return CHOICES[randomNumber];
+};
+
+// Function to get hummans choice by prompt (rock, paper or scissors)
+const getHumanChoice = () => {
+  let humanChoice = window.prompt("Enter your choice");
+  return humanChoice;
+};
+
+// logic to play a single round
+const playRound = (humanChoice, computerChoice) => {
+  let humanChoiceLowerCase = humanChoice.toLowerCase();
+  let humanChoiceIndex = CHOICES.indexOf(humanChoiceLowerCase);
+  let computerChoiceIndex = CHOICES.indexOf(computerChoice);
+  // win the last index instead of 1st index
+  // win the smaller index
+  // tie if both index are same
+  if (humanChoiceIndex == 0 && computerChoiceIndex == CHOICES.length - 1) {
+    console.log(`You Lose!  ${computerChoice} beats ${humanChoiceLowerCase}`);
+    return ++computerScore;
+  } else if (
+    humanChoiceIndex == CHOICES.length - 1 &&
+    computerChoiceIndex == 0
+  ) {
+    console.log(`You Win!  ${computerChoice} beats ${humanChoiceLowerCase}`);
+    return ++humanScore;
+  } else if (humanChoiceIndex < computerChoiceIndex) {
+    console.log(`You Win!  ${computerChoice} beats ${humanChoiceLowerCase}`);
+    return ++humanScore;
+  } else if (humanChoiceIndex > computerChoiceIndex) {
+    console.log(`You Lose!  ${computerChoice} beats ${humanChoiceLowerCase}`);
+    return ++computerScore;
+  } else {
+    console.log("tie");
+    return;
+  }
+};
+
+//playRound(humanSelection, computerSelection);
+
+const playGame = (round) => {
+  let i = 0;
+  while (i < round) {
+    playRound(getHumanChoice(), getComputerChoice());
+    i++;
+  }
+  return (winner =
+    humanScore === computerScore
+      ? "Finally Tie"
+      : humanScore > computerScore
+      ? "Finally You are winner"
+      : "Finally You lose! try again later");
+};
+console.log(playGame(5));
+console.log("scor=", humanScore, computerScore);
